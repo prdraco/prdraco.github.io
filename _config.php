b8aa4cc2b0ed07ac1312b8c4e7d874348f2792f0
Robert Padar <63544820+prdraco@users.noreply.github.com>
2023-09-08 20:50:18 +0100

    Add files via upload

---
+++ _config.php
@@ -0,0 +1,39 @@
+<?php 
+
+$websiteTitle = "Animes House"; // Website Name
+$websiteUrl = "//{$_SERVER['SERVER_NAME']}";  // Website URL
+$websiteLogo = "/Logo.png"; // Logo
+$websiteLogo2 = "/Logo.png";
+$contactEmail = "senku@animeshouse.live"; // Contact Email
+
+
+//Donate 
+$donate = "#";
+
+// Socials 
+$telegram = "https://telegram.me/@allwishme"; // telegram
+$discord = "#"; // Discord
+$redit = "#"; // Reddit
+$twitter = "#"; // Twitter
+ 
+
+
+$disqus = "https://watchzia.disqus.com"; // Disqus
+
+$cdn = "https://animeimages-7tqpw0gvd-junaed537-gmailcom.vercel.app/"; // cdn repo https://github.com/warlordsnet/cdnzia
+
+// API URL
+$api ="https://animezia.onrender.com"; // api repo https://github.com/warlordsnet/zia-api
+
+$imgk = "ani1"; // your imagekit id
+
+ $iani = "hxxpz:qqapi*c=nzu?ex*=rg";
+ $s1 = str_replace("x","t",$iani);
+ $s2 = str_replace("z","s",$s1);
+ $s3 = str_replace("q","/",$s2);
+ $s4 = str_replace("*",".",$s3);
+ $s5 = str_replace("?","m",$s4);
+ $ani = str_replace("=","o",$s5);
+
+$banner = "https://cdnzia.pages.dev/images/banner.webp";  //Banner
+?>
